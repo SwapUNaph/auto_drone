ecf67773c64f97a76bc317066cb1e03ad58ae0e0
SwapUNaph <naphadeswapneel@gmail.com>
2016-02-11 13:02:26 -0500

    Testing with Calicam

--- scripts/gate_detection_and_estimation.py
+++ scripts/gate_detection_and_estimation.py
@@ -30,12 +30,12 @@ from cv_bridge import CvBridge, CvBridgeError
 ######################  Detection Parameters  ##########################
 
 # Gate size in meters
-GATE_SIZE = 1.15
+GATE_SIZE = 1.25
 NOT_GATE_SIZE = 0.8
 
 
 # HSV thresholds for LED gate
-hsv_thresh_low = (0, 0, 200)
+hsv_thresh_low = (0, 0, 250)
 hsv_thresh_high = (180, 255, 255)
 
 # HSV thresholds for non-LED gate
@@ -43,8 +43,8 @@ not_gate_hsv_thresh_low = (0, 0, 0)
 not_gate_hsv_thresh_high = (180, 255, 100)
 
 # Gate thresholds
-AREA_THRESH = 1000
-ASPECT_RATIO_THRESH_LOW = 0.5 # Should be between 0.0 and 1.0
+AREA_THRESH = 2000
+ASPECT_RATIO_THRESH_LOW = 0.7 # Should be between 0.0 and 1.0
 ASPECT_RATIO_THRESH_HIGH = 1/ASPECT_RATIO_THRESH_LOW
 SOLIDITY_THRESH = 0.90
 ROI_MEAN_THRESH = 100
@@ -58,24 +58,24 @@ GATE_TYPE_VERTICAL = None
 
 # Camera Capture
 cap = cv2.VideoCapture(0)
-cap.set(3, 2560) # Width 2560x720
-cap.set(4, 720) # Height
-cap.set(5, 60) # FPS
+cap.set(3, 640) # Width 2560x720
+cap.set(4, 480) # Height
+cap.set(5, 30) # FPS
 FRAME_WIDTH = cap.get(3)
 
 bridge = CvBridge()
 
-drone_position = np.zeros(3)
-drone_orientation = np.zeros(3)
-drone_linear_vel = np.zeros(3)
-drone_angular_vel = np.zeros(3)
+drone_position = np.zeros(3, float)
+drone_orientation = np.zeros(3, float)
+drone_linear_vel = np.zeros(3, float)
+drone_angular_vel = np.zeros(3, float)
 
 ########################################################################
 
 ##########################  Filters   ##################################
 # MVA filters
 orientationFilter = MVA(20)
-translationFilter = MVA(20)
+#translationFilter = MVA(15)
 
 # Loop Frequency (important for kalman filtering)
 LOOP_FREQ = 60
@@ -112,22 +112,23 @@ def jacobianB(X,U):
                     [0,0,0,0,-1,0],
                     [0,0,0,0,0,-1]] )
  
-# C,Q,R
-C = np.eye(6, dtype='float')      
-Q = np.zeros((6, 6), float)
+# A,B,C,Q,R
+A = np.zeros((3, 3), float)
+B = -np.eye(3, dtype='float')
+C = np.eye(3, dtype='float')      
+Q = np.zeros((3, 3), float)
 np.fill_diagonal(Q, PROCESS_NOISE)
 
 
 R = np.zeros((C.shape[0], C.shape[0]), float)
 np.fill_diagonal(R, SENSOR_NOISE)
-R[5,5] = 0.05
 
 # Initial Estimate
-X0 = np.zeros((6,1))
+X0 = np.zeros((3,1))
 
-# Instantiate EKF
-EKF = ExtendedKalmanFilter(gatePoseDynamics, jacobianA, jacobianB, C, Q, R, X0, dt=1.0/LOOP_FREQ)
-
+# Instantiate KF
+#KF = ExtendedKalmanFilter(gatePoseDynamics, jacobianA, jacobianB, C, Q, R, X0, dt=1.0/LOOP_FREQ)
+KF = KalmanFilter(A,B,C,Q,R,X0,dt=0.1)
 ########################################################################
 	
 def signal_handler(_, __):
@@ -281,17 +282,17 @@ def getGatePose(contour, gate_side):
     dRc = np.array([[0,0,1],[-1,0,0],[0,-1,0]])
 
     ############################  For CaliCam    ##################################
-    # cameraMatrix = np.array([[ 258.58131479, 0.0 , 348.1852167 ], [0.0 , 257.25344992, 219.07752178], [0.0 , 0.0, 1.0]])
-    # distCoeffs = np.array([[-0.36310169,  0.10981468,  0.0057042,  -0.001884,   -0.01328491]])
+    cameraMatrix = np.array([[ 258.58131479, 0.0 , 348.1852167 ], [0.0 , 257.25344992, 219.07752178], [0.0 , 0.0, 1.0]])
+    distCoeffs = np.array([[-0.36310169,  0.10981468,  0.0057042,  -0.001884,   -0.01328491]])
 
     ###############################################################################
 
     ####################################   For ZED stereo   #############################
     ## Camera Matrix 720p 
-    cameraMatrix = np.array([[700, 0.0, 640], [0.0, 700, 360], [0.0, 0.0, 1.0]])
+    #cameraMatrix = np.array([[700, 0.0, 640], [0.0, 700, 360], [0.0, 0.0, 1.0]])
 
     ### Distortion Coefficients 720p
-    distCoeffs = np.array([-0.1740500032901764, 0.028304599225521088, 0.0, 0.0, 0.0])
+    #distCoeffs = np.array([-0.1740500032901764, 0.028304599225521088, 0.0, 0.0, 0.0])
 
     # Camera Matrix VGA
     #cameraMatrix = np.array([[350, 0.0, 336], [0.0, 350, 188], [0.0, 0.0, 1.0]])
@@ -368,7 +369,8 @@ if __name__ == '__main__':
         if img is None:
             rospy.logerr("No Camera detected!!!")
         else:
-            img = img[:,:int(FRAME_WIDTH/2)]
+            # For Zed Stereo
+            #img = img[:,:int(FRAME_WIDTH/2)]
             
             # Gate detection
             
@@ -379,33 +381,33 @@ if __name__ == '__main__':
                 cv2.drawContours(img, [gate.reshape(4,2)], 0, (255, 0, 0), 2)
                 annotateCorners(gate, img)  
                 raw_euler, raw_tvec = getGatePose(gate, GATE_SIZE)
-                EKF.C = np.eye(6, dtype='float') # Use measurements to fuse with predictions
+                KF.C = np.eye(KF.C.shape[0], dtype='float') # Use measurements to fuse with predictions
             else:
-				raw_euler = np.zeros(3)
-				raw_tvec = np.zeros(3)
-				EKF.C = np.zeros((6,6), float) # No measurements, rely only on prediction
+				raw_euler = np.zeros(3, float)
+				raw_tvec = np.zeros(3, float)
+				KF.C = np.zeros(KF.C.shape, float) # No measurements, rely only on prediction
                 
 			# Kalman Filtering
             #euler = orientationFilter.update(raw_euler)
             #tvec = translationFilter.update(raw_tvec)
             
-            Y = np.append(raw_tvec, raw_euler).reshape((6,1))
-            U = np.append(drone_linear_vel, drone_angular_vel).reshape((6,1))
-            EKF.dt = (time() - start)
-            EKF.filter(U,Y)
-            X = EKF.X.copy()
-            tvec = X[:3]
-            euler = X[3:]
-
+            Y = raw_tvec.reshape((3,1))
+            U = drone_linear_vel.reshape((3,1))
+            KF.dt = (time() - start)
+            KF.filter(U,Y)
+            X = KF.X.copy()
+    
+            euler = orientationFilter.update(raw_euler)
+            
             #logMeasuredGatePose(X)
-            filtered_gate_WP = array2WP(tvec, euler[2], "")
+            filtered_gate_WP = array2WP(X, euler[2], "")
             gate_WP = array2WP(raw_tvec, raw_euler[2], "")
             gate_pose_pub.publish(gate_WP)
             filtered_gate_pose_pub.publish(filtered_gate_WP) 
             
             rospy.loginfo("LOOP Time: {} s".format(time() - start))
-            #img = cv2.resize(img, None, fx=0.1, fy=0.1)   
-            #image_publisher.publish(bridge.cv2_to_imgmsg(img,"bgr8"))
+            #img = cv2.resize(img, None, fx=0.25, fy=0.25)   
+            image_publisher.publish(bridge.cv2_to_imgmsg(img,"bgr8"))
          
         #rate.sleep()
             
